763972145176eb11dd2f6991424cc59093d9706b
Niccolo Brogi <me@nbrogi.com>
2019-01-05 21:55:09 +0100

    Update 01-prepare.sh

--- ubuntu-bionic/01-prepare.sh
+++ ubuntu-bionic/01-prepare.sh
@@ -5,4 +5,4 @@ sudo apt-get full-upgrade -y
 sudo apt-get autoremove -y
 
 # set timezone (keep UTC default)
-sudo (export DEBIAN_FRONTEND=noninteractive; apt-get install tzdata -y)
+(export DEBIAN_FRONTEND=noninteractive; sudo apt-get install tzdata -y)
