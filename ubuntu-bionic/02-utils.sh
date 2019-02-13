9ce434ccbc03e941f3f26b46306f8b8dbfac1b91
Niccolo Brogi <me@nbrogi.com>
2019-02-13 23:04:30 +0100

    Update 02-utils.sh

--- ubuntu-bionic/02-utils.sh
+++ ubuntu-bionic/02-utils.sh
@@ -3,4 +3,5 @@ sudo apt-get install git htop tree -y
 
 # install other utils
 sudo apt-get install net-tools -y # provides "ifconfig" command, etc.
+sudo apt install software-properties-common # to add PPAs
 sudo apt-get install apt-utils -y
